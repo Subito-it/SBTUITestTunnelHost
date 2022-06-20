16aaab1fdc75dd0f7888f2f3218561b1c2a06bb0
Tomas Camin <tomas.camin@adevinta.com>
2022-06-20 18:03:56 +0200

    Ignore Pods folders

--- Example/Pods/Target Support Files/Pods-SBTUITestTunnelHost_ExampleUITests/Pods-SBTUITestTunnelHost_ExampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SBTUITestTunnelHost_ExampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SBTUITestTunnelHost_ExampleUITests
-@end
--- SBTUITunnelHostServer/Pods/Target Support Files/Pods-SBTUITunnelHostServer/Pods-SBTUITunnelHostServer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SBTUITunnelHostServer : NSObject
-@end
-@implementation PodsDummy_Pods_SBTUITunnelHostServer
-@end
