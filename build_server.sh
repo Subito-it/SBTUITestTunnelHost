e15832a71485d42925782b4500dd6e83865d617e
Tomas Camin <tomas.camin@adevinta.com>
2022-12-29 12:29:31 +0100

    Add build script

---
+++ build_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if ! command -v xcodegen &> /dev/null
+then
+    echo "XcodeGen not found!"
+    exit
+fi
+
+rm -rf SBTUITestTunnelServer.app &>/dev/null; 
+
+cd SBTUITunnelHostServer; xcodegen &>/dev/null && xcodebuild -workspace SBTUITestTunnelServer.xcworkspace -scheme SBTUITestTunnelServer -derivedDataPath build -configuration Release &>/dev/null && cp -R build/Build/Products/Release/SBTUITestTunnelServer.app .. && rm -rf build && cd .. && open .
+
+if test $?
+then
+    echo "Server app SBTUITestTunnelServer.app successfully built!"
+else
+    echo "Build failed"
+fi
